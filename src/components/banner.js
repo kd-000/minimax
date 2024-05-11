abf137c4025996166f9863395d6065c13a731124
kd-000 <kday.home@gmail.com>
2024-05-11 23:52:48 +0100

    Added github banner

---
+++ src/components/banner.js
@@ -0,0 +1,11 @@
+import '../css/banner.css';
+
+function Banner() {
+  return (
+    <div className="banner-outer">
+        <a href="https://github.com/kd-000/minmax"><img className="icon" src="github-mark.svg" alt="GitHub Logo Link"></img></a>
+    </div>
+  );
+}
+
+export default Banner;
