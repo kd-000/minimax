481d96e6da896d77972db657ca6a9f331772b7f3
kd-000 <kday.home@gmail.com>
2024-05-20 17:17:12 +0100

    Added security to open in new tab

--- src/components/banner.js
+++ src/components/banner.js
@@ -3,7 +3,7 @@ import '../css/banner.css';
 function Banner() {
   return (
     <div className="banner-outer">
-        <a href="https://github.com/kd-000/minmax"><img className="icon" src="github-mark.svg" alt="GitHub Logo Link"></img></a>
+        <a href="https://github.com/kd-000/minimax" target="_blank" rel="noopener noreferrer"><img className="icon" src="github-mark.svg" alt="GitHub Logo Link"></img></a>
     </div>
   );
 }
