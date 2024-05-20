41ae8a461d1160f55ba15d5b5d0c068606f14224
kd-000 <kday.home@gmail.com>
2024-05-20 17:15:58 +0100

    Added window height view

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 import './css/App.css';
 import GraphView from './components/GraphView';
 import Banner from './components/banner'
+import BackButton from './components/backButton';
+import {react,useState, useEffect} from 'react'
 
 function App() {
+  const [windowHeight, setWindowHeight] = useState(window.innerHeight - 50);
+
+  useEffect(() => {
+    const handleResize = () => {
+      setWindowHeight(window.innerHeight - 50);
+    };
+    window.addEventListener('resize', handleResize);
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-    <div className="outer-display">
+      <div className="outer-display" style={{ height: windowHeight }}>
+      <BackButton></BackButton>
       <header className="title-header">
         Minimax & Alpha Beta Pruning
       </header>
@@ -15,7 +30,7 @@ function App() {
         <GraphView></GraphView>
       </div>
       <Banner></Banner>
-    </div>
+      </div>
   );
 }
 
